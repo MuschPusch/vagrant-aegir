5aee2b175398686f036d2e7ef1f98872b24960e4
muschpusch <volkan@rhizom.nl>
2014-01-13 17:39:36 +0100

    conditional append of wait for NFS + enabling php extension for adding curl or other stuff

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -23,15 +23,21 @@ node 'aegir.local' {
     platform_path  => '/var/aegir/hostmaster-6.x-2.x',
     require        => Class['drush::git::drush'],
   }
-  include php
+
+
+  exec { "grep":
+    command => "grep -q 'sleep 8; apache2ctl start' /etc/init.d/rc.local || /bin/echo 'sleep 8; apache2ctl start' >> /etc/init.d/rc.local",
+    require => Class['aegir::dev'],
+  }
+
+  class{ 'php':
+    require => Class['drush::git::drush'],
+  }
 }
  
 # we need to wait a bit since apache should only start when NFS is enabled
 # otherwise some .htaccess files which are required won't be available
-exec { "echo":
-  command => "/bin/echo 'sleep 5 ; apache2 start' >> /etc/init.d/rc.local ",
-  require => Class['aegir::dev'],
-}
+
 
 # This is in the defaults that is included in aegir::platform...
 # I'm not sure why it isn't inherited properly from there.
