da7ae74b02e4124af0ae8e3503a80fc1ed97ab0e
muschpusch <volkan@rhizom.nl>
2014-03-21 15:20:59 +0100

    logger is installed by default

--- puppet/modules/rc/manifests/init.pp
+++ puppet/modules/rc/manifests/init.pp
@@ -1,14 +1,8 @@
 class rc {
-#we need to do this for every user
-# or we change vimrc to not being global anymore
+  # for sure we have logger and RC on this distros
   if ($operatingsystem == "Ubuntu") or ($operatingsystem == "Debian") {
-    package { "logger":
-      ensure => present,
+    file { "/etc/rc.local":
+      content => template("rc/rc.local"),
     }
-  }
-
-  file { "/etc/rc.local":
-    content => template("rc/rc.local"),
-    require => Package['logger'],
   }
 }
